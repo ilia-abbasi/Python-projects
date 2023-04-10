416b8d6a092959c39e8170444f20846df2d70dc5
Ilia <81512402+ilia-abbasi@users.noreply.github.com>
2023-04-10 20:06:31 +0330

    Create STH maker.py

---
+++ STH/STH maker.py
@@ -0,0 +1,43 @@
+#STH (SeTil Hash) algorithm
+#Designer: Ilia Abbasi
+
+BASE = 16
+DIGEST_LENGTH = 64
+
+def STH(s : str) -> str:
+	
+	pieces = ['N', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'L', 'R', 'E', 'X', 'T']
+	chars = []
+	digest = ""
+	
+	for i in range(len(s)):
+		chars.append(ord(s[i]) % BASE)
+	
+	for i in range(DIGEST_LENGTH):
+		x = (chars[i] * (len(chars) + 23) * chars[len(chars) - 1] + 5) % 2221
+		chars.append(x)
+	
+	
+	itr = 0;
+	while len(chars) > DIGEST_LENGTH:
+		if itr >= DIGEST_LENGTH:
+			itr = 0
+		
+		x = ((chars[len(chars) - 2] * chars[len(chars) - 1] * len(chars)) - 2) % 43 + 1
+		chars[itr] = x
+		chars.pop()
+		itr += 1
+	
+	for i in range(DIGEST_LENGTH):
+		digest += pieces[chars[i] % BASE + 1];
+	
+	
+	return digest
+
+def main():
+    while 1:
+        plain = input()
+        print(STH(plain));
+
+if __name__ == "__main__":
+    main()
